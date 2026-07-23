ef3231cb1c8b9d007ed6e727a90572cb2ae68a34
Akbar Muxiddinov <akbarxojamuxiddinov8@gmail.com>
2026-07-23 20:55:51 +0500

    Add files via upload
    Первая версия проекта Telegram-бота.

--- main.py
+++ main.py
@@ -1,233 +1,196 @@
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, CallbackContext, \
-    ConversationHandler
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters, ConversationHandler, \
+CallbackContext
+import db
 
-import sqlite3
+TOKEN = 'TOKEN'
 
-TOKEN = "ADMIN TOKEN"
-ADMIN_ID = "ADMIN ID"
-
-db = sqlite3.connect("movies.db", check_same_thread=False)
-cursor = db.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS movies (
-    code TEXT PRIMARY KEY,
-    file_id TEXT,
-    caption TEXT
-);
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS channels (
-    channel TEXT PRIMARY KEY
-);
-""")
-db.commit()
-
-MOVIE_CODE, MOVIE_FILE, MOVIE_CAPTION = range(3)
-
-
-def get_channels():
-    cursor.execute("SELECT channel FROM channels")
-    return [i[0] for i in cursor.fetchall()]
-
-
-def check_subscription(bot, user_id):
-    channels = get_channels()
-
-    if not channels:
-        return True
-
-    for channel in channels:
-        try:
-            member = bot.get_chat_member(channel, user_id)
-            if member.status in ['left', 'kicked']:
-                return False
-        except:
-            return False
-    return True
-
-
-def subscription_keyboard():
-    keyboard = []
-
-    for ch in get_channels():
-        if ch.startswith("@"):
-            url = f"https://t.me/{ch[1:]}"
-        else:
-            url = ch
-
-        keyboard.append([
-            InlineKeyboardButton(f"{ch}", url=url)
-        ])
-    keyboard.append([
-        InlineKeyboardButton("Tekshirish", callback_data="check_sub")
-    ])
-    return InlineKeyboardMarkup(keyboard)
+(
+    NAME,
+    PHONE,
+    LOCATION,
+    MAIN_MENU,
+    EDIT_NAME,
+    EDIR_PHONE,
+    SETTINGS_MENU,
+    FOOD_MENU
+) = range(8)
 
 
 def start(update: Update, context: CallbackContext):
-    user_id = update.message.from_user.id
+    user = db.get_user(update.effective_user.id)
 
-    if not check_subscription(context.bot, user_id):
-        update.message.reply_text("Botdan foydalanish uchun kanallarga obun bo'ling !",
-                                  reply_markup=subscription_keyboard())
-        return
+    if user:
+        return main_menu(update, context)
 
-    update.message.reply_text("Assalomu Alaykum\n"
-                              "Kino kodini kiriting: ")
+    update.message.reply_text("👤 Ism Familya kiriting: ")
+    return NAME
 
 
-def get_movies(update: Update, context: CallbackContext):
-    user_id = update.message.from_user.id
-
-    if not check_subscription(context.bot, user_id):
-        update.message.reply_text("Botdan foydalanish uchun kanallarga obun bo'ling !",
-                                  reply_markup=subscription_keyboard())
-        return
-
-    code = update.message.text.strip()
-    cursor.execute("SELECT file_id, caption FROM movies WHERE code = ?", (code,))
-    result = cursor.fetchone()
-
-    if result:
-        file_id, caption = result
-        update.message.reply_video(video=file_id, caption=caption)
-    else:
-        update.message.reply_text("Kchirasiz, ko notog'ri !")
+def get_name(update, context):
+    context.user_data['name'] = update.message.text
+    update.message.reply_text("📞 Telefon raqamingizni yuboring: ",
+                              reply_markup=ReplyKeyboardMarkup(
+                                  [[KeyboardButton("Raqamingizni kiriting 📱", request_contact=True)]],
+                                  resize_keyboard=True
+                              ))
+    return PHONE
 
 
-def check_subscription_callback(update: Update, context: CallbackContext):
-    query = update.callback_query
-    user_id = query.from_user.id
-    query.answer()
+def get_phone(update, context):
+    context.user_data['phone'] = update.message.contact.phone_number
+    update.message.reply_text("📍 Manzilingizni kiriting: ",
+                              reply_markup=ReplyKeyboardMarkup(
+                                  [[KeyboardButton("📍 Manzilingizni yuborish", request_location=True)]],
+                                  resize_keyboard=True
+                              ))
+    return LOCATION
 
-    if check_subscription(context.bot, user_id):
-        query.message.edit_text(
-            "Obuna tasdiqlandi\n\n"
-            "Endi kino kodini yuklanb olishingiz mumkin !"
+
+def get_location(update, context):
+    loc = update.message.location
+
+    db.add_user(
+        update.effective_user.id,
+        context.user_data['name'],
+        context.user_data['phone'],
+        loc.latitude,
+        loc.longitude
+    )
+
+    update.message.reply_text("Ro'yhatdan o'tdingiz ✅")
+    return main_menu(update, context)
+
+
+
+
+
+def main_menu(update, context):
+    update.message.reply_text("Asosiy menu: ",
+                              reply_markup=ReplyKeyboardMarkup(
+                                  [
+                                      ["📋 Menyu", "🛒 Savat"],
+                                      ["⚙️ Sozlamalar"],
+                                      ["💬 Izoh qoldirish"]
+                                  ],
+                                  resize_keyboard=True
+                              ))
+    return MAIN_MENU
+
+
+def main_menu_select(update, context):
+    text = update.message.text
+
+    if text == "📋 Menyu":
+        return food_menu(update, context)
+    elif text == "⚙️ Sozlamalar":
+        return settings_menu(update, context)
+    elif text == "💬 Izoh qoldirish":
+        update.message.reply_text("Izoh yozish mumkin emas, profilaktikajarayonida !")
+        return MAIN_MENU
+
+
+def food_menu(update, context):
+    update.message.reply_text(
+        "Menyulardan birini tanlang: ",
+        reply_markup=ReplyKeyboardMarkup(
+            [
+                ["Lavashlar 🌯", "Burgerlar 🍔"],
+                ['Shashliklar 🍡', 'Sushilar 🍣'],
+                ['⬅️ Orqaga'],
+            ],
+            resize_keyboard=True
         )
-    else:
-        query.answer(
-            "Hali barcha kanallarga obuna bo'lmagansiz !",
-            show_alert=True
-        )
+    )
+    return FOOD_MENU
 
 
-def add_channels(update: Update, context: CallbackContext):
-    if update.message.from_user.id != ADMIN_ID:
-        return
+def food_menu_select(update, context):
+    text = update.message.text
 
-    if not context.args:
-        update.message.reply_text("Misol: /addchannels @kanal")
-        return
+    if text == "Lavashlar 🌯":
+        update.message.reply_text("Lavash bor bu yerda")
 
-    channel = context.args[0]
-    cursor.execute("INSERT OR IGNORE INTO channels VALUES (?)", (channel,))
-    db.commit()
+    elif text == "Burgerlar 🍔":
+        update.message.reply_text("Burgerlar bor bu yerda")
 
-    update.message.reply_text(f"Kanal qo'shildi: {channel}")
+    elif text == "Shashliklar 🍡":
+        update.message.reply_text("Shashliklar bor bu yerda")
 
+    elif text == "Sushilar 🍣":
+        update.message.reply_text("Sushilar bor bu yerda")
 
-def delete_channels(update: Update, context: CallbackContext):
-    if update.message.from_user.id != ADMIN_ID:
-        return
-
-    if not context.args:
-        update.message.reply_text("Misol: /delchanel @kanal")
-        return
-
-    channel = context.args[0]
-    cursor.execute("DELETE FROM channels WHERE channel=?", (channel,))
-    db.commit()
-
-    update.message.reply_text(f"Kanal o'chirildi: {channel}")
+    elif text == "⬅️ Orqaga":
+        main_menu(update, context)
 
 
-def list_channels(update: Update, context: CallbackContext):
-    if update.message.from_user.id != ADMIN_ID:
-        return
-
-    channels = get_channels()
-    if not channels:
-        update.message.reply_text("Kanallar yo'q !")
-
-    text = "Kanallar ro'yhati\n\n"
-    for ch in channels:
-        text += f"{ch}\n"
-
-    update.message.reply_text(text)
-    return
+def settings_menu(update, context):
+    update.message.reply_text("Ma'lumotlarni tahrirlash: ",
+                              reply_markup=ReplyKeyboardMarkup(
+                                  [
+                                      ["✏️ Ism familya"],
+                                      ['📞 Telefon raqam'],
+                                      ['⬅️ Orqaga'],
+                                  ],
+                                  resize_keyboard=True
+                              ))
+    return SETTINGS_MENU
 
 
-def admin_start(update: Update, context: CallbackContext):
-    if update.message.from_user.id != ADMIN_ID:
-        update.message.reply_text("Siz admin emassiz")
-        return
+def settings_select(update, context):
+    text = update.message.text
 
-    update.message.reply_text("Kino kodini kiriritng: ")
-    return MOVIE_CODE
+    if text == "✏️ Ism familya":
+        update.message.reply_text("✏️ Yangi ism familyani kiriting: ")
+        return EDIT_NAME
 
+    elif text == "📞 Telefon raqam":
+        update.message.reply_text("📞 Yangi raqamingini kiriting: ", reply_markup=ReplyKeyboardMarkup(
+            [[KeyboardButton("Raqamni yuborish", request_contact=True)]],
+            resize_keyboard=True
+        ))
+        return EDIR_PHONE
 
-def admin_code(update: Update, context: CallbackContext):
-    context.user_data['code'] = update.message.text.strip()
-    update.message.reply_text("Endi kino yoki video yuklang: ")
-    return MOVIE_FILE
-
-
-def admin_file(update: Update, context: CallbackContext):
-    if not update.message.video:
-        update.message.reply_text("Faqat video yuboaring !")
-        return MOVIE_FILE
-
-    context.user_data['file_id'] = update.message.video.file_id
-    update.message.reply_text("Video tagidagi matinni yunoring...")
-    return MOVIE_CAPTION
+    elif text == "⬅️ Orqaga":
+        return main_menu(update, context)
 
 
-def admin_caption(update: Update, context: CallbackContext):
-    cursor.execute("""
-    INSERT OR REPLACE INTO movies VALUES (?, ?, ?)
-    """, (
-        context.user_data['code'],
-        context.user_data['file_id'],
-        update.message.text
-    ))
-    db.commit()
-
-    update.message.reply_text("Kino saqlandi !")
-    return ConversationHandler.END
+def edit_name(update, context):
+    db.update_name(update.effective_user.id, update.message.text)
+    update.message.reply_text("Ism Familya muvofaqiyatli o'zgartirildi ✅")
+    return main_menu(update, context)
 
 
-def cancel(update: Update, context: CallbackContext):
-    update.message.reply_text("Jarayon bekor qilindi !")
-    return ConversationHandler.END
+def edit_phone(update, context):
+    db.update_phone(update.effective_user.id, update.message.contact.phone_number)
+    update.message.reply_text("Telefon raqam muvofaqiyatli o'zgartirildi ✅")
+    return main_menu(update, context)
 
 
 def main():
+    db.crate_table()
+
     updater = Updater(TOKEN)
     dp = updater.dispatcher
 
-    admin_conv = ConversationHandler(
-        entry_points=[CommandHandler("admin", admin_start)],
+    conv = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
         states={
-            MOVIE_CODE: [MessageHandler(Filters.text & ~Filters.command, admin_code)],
-            MOVIE_FILE: [MessageHandler(Filters.video & ~Filters.command, admin_file)],
-            MOVIE_CAPTION: [MessageHandler(Filters.text, admin_caption)],
+            NAME: [MessageHandler(Filters.text, get_name)],
+            PHONE: [MessageHandler(Filters.contact, get_phone)],
+            LOCATION: [MessageHandler(Filters.location, get_location)],
+
+            MAIN_MENU: [MessageHandler(Filters.text, main_menu_select)],
+            SETTINGS_MENU: [MessageHandler(Filters.text, settings_select)],
+            FOOD_MENU: [MessageHandler(Filters.text, food_menu_select)],
+
+            EDIT_NAME: [MessageHandler(Filters.text, edit_name)],
+            EDIR_PHONE: [MessageHandler(Filters.contact, edit_phone)],
         },
-        fallbacks=[CommandHandler("cancel", cancel)]
+        fallbacks=[],
     )
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("addchannel", add_channels))
-    dp.add_handler(CommandHandler("delchannel", delete_channels))
-    dp.add_handler(CommandHandler("channel", list_channels))
-
-    dp.add_handler(admin_conv)
-    dp.add_handler(CallbackQueryHandler(check_subscription_callback, pattern="check_sub"))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, get_movies))
-
+    dp.add_handler(conv)
     updater.start_polling()
     updater.idle()
 
